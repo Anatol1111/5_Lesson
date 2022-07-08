aa4efc14514036d969edbdfb6d8e594844abbf0e
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-09 00:52:12 +0600

    homework_1 modiffied

---
+++ Lesson_5/Homework_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(99, 1000);
+    }
+    return arr;
+}
+void evenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+            count++;
+
+    Console.WriteLine($"всего {arr.Length} чисел, {count} из них чётные");
+}
+
+int[] arr_1 = MassNums(5);
+Print(arr_1);
+evenNum(arr_1);
